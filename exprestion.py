9699bc6b8dc35c7576b28170a07c560a8574b51f
Harsh Prasad <harshprasad2408@gmail.com>
2025-02-20 00:20:55 +0530

    Add files via upload

---
+++ exprestion.py
@@ -0,0 +1,42 @@
+#type 1
+a,b=2,3
+txt="aa"
+print(2*txt*3)
+ 
+#type 2
+c,d="3",4
+txtt="a"
+print((c+txtt)*d)
+
+#type 3
+e,f=5,5
+g=12
+print(e*g+f)
+
+#type 4
+h,i=5.20,10
+print(h*i)
+
+#type 6
+j,k=2,3
+print(j/k)
+
+#type 7
+l,m=12,-5
+print(l/m , l//m)
+
+#type 8
+n,o=5,-2
+print(n%o)
+
+
+#type 9
+p,q=-16,6
+print(p%q)
+
+#type 10
+r,s=11,6
+print(r%s)
+
+#type 11
+t,u=10,-5
